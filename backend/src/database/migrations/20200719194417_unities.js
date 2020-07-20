82f2afc3d8617190a2c5be014e6f5d397205456d
joandersonSJ <joandersonsantosdev@gmail.com>
2020-07-19 21:08:19 -0300

    :zap: migrate das unidades criada

---
+++ backend/src/database/migrations/20200719194417_unities.js
@@ -0,0 +1,25 @@
+
+exports.up = async function(knex) {
+  return await knex.schema.createTable('unidades', (table) =>{
+
+    table.increments('id_unidade');
+    table.string('municipio',50).unique().notNullable();
+    table.integer('inscricao_mte').unique().notNullable();
+    table.string('nome_unidade',100).unique().notNullable();
+    table.string('sec_pasta',100).unique().notNullable();
+    table.string('servicos',100).unique().notNullable();
+    table.string('imovel',50).unique().notNullable();
+    table.string('tipo_imovel',50).unique().notNullable();
+    // table.time('horario_funcionamento').notNullable();
+    table.string('tipo_unidade',10).notNullable();
+
+
+    
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = async function(knex) {
+  return await knex.schema.dropTable('unidades');
+};
